dfe93c11dc9a6210f6717bb9f43073912208566e
Evan Yang <evanyang@pop-os.localdomain>
2020-02-04 11:22:46 -0800

    added arrays to localStorage (radius)

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,18 +3,25 @@ document.addEventListener('DOMContentLoaded', () => {
   let instances = M.Dropdown.init(elems)
 })
 
+const pushRadiusToRadiusArray = (rad) => {
+  let arr = JSON.parse(localStorage.getItem('radius')) || []
+  console.log(arr)
+  arr.push(rad)
+  localStorage.setItem('radius', JSON.stringify(arr))
+}
+
 document.getElementById('m-five').addEventListener('click', () => {
-  localStorage.setItem('radius', 5)
+  pushRadiusToRadiusArray(5)
 })
 
 document.getElementById('m-ten').addEventListener('click', () => {
-  localStorage.setItem('radius', 10)
+  pushRadiusToRadiusArray(10)
 })
 
 document.getElementById('m-twentyfive').addEventListener('click', () => {
-  localStorage.setItem('radius', 25)
+  pushRadiusToRadiusArray(25)
 })
 
 document.getElementById('m-fifty').addEventListener('click', () => {
-  localStorage.setItem('radius', 50)
+  pushRadiusToRadiusArray(50)
 })
