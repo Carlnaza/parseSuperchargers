566a684acc9276b7af32e0fac7470249e0d53761
Carlo <csnazareno94@gmail.com>
2020-02-06 14:09:21 -0800

    cleaned up weather api

--- assets/js/app.js
+++ assets/js/app.js
@@ -79,26 +79,14 @@ document.getElementById('car-details-btn').addEventListener('click', _ => {
 
 document.getElementById('searchCity').addEventListener('click', event => {
   event.preventDefault()
-
   document.getElementById('displayWeather').innerHTML = ``
-
-  fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${document.getElementById('city').value}&APPID=e2bb961f87ee45b7f3164d336e92a97a&units=imperial`)
-    .then(r => r.json())
-    .then(({ list }) => {
-
   fetch(`https://api.weatherapi.com/v1/forecast.json?key=ea4d3d5c304c48499f2204108200502&q=${document.getElementById('city').value}&days=5`)
       .then(r => r.json())
-      .then(({ location, current, forecast, condition }) => {
+      .then(weather => {
 
         document.getElementById('displayWeather').innerHtml = ``
 
           for (var i = 0; i < 5; i++) {
-
-            let forecastInfo = forecast.forecastday[i]
-            let daily = forecastInfo.day
-            let date = moment(`${forecastInfo.date}`, 'YYYY-MM-DD').format('MMM-DD-YYYY');
-            let listInfo = list[i]
-            console.log(current.condition.icon)
             let weatherElem = document.createElement('div')
             weatherElem.innerHTML = `
         
@@ -106,16 +94,17 @@ document.getElementById('searchCity').addEventListener('click', event => {
         
           <div class="row">
             <div class="col s6">
-              <h5>${location.name}
-              <h6>${date}</h6>
-              <img src="http://openweathermap.org/img/w/${listInfo.weather[0].icon}.png">
+              <h5>${weather.location.name}, ${weather.location.region}</h5>
+              <h6>${weather.current.condition.text}</h6>
+              <img src="https:${weather.current.condition.icon}">
             </div>
             
             <div class="col s6">
-              <h6>High: ${daily.maxtemp_f}</h6>
-              <h6>Low: ${daily.mintemp_f}</h6>
-              <h6>Humidity: ${daily.avghumidity}</h6>
-              <h6>Maximum Windspeed: ${daily.maxwind_mph}</h6>
+              <h6>Date: ${weather.forecast.forecastday[i].date}</h6>
+              <h6>High: ${weather.forecast.forecastday[i].day.maxtemp_f}°</h6>
+              <h6>Low: ${weather.forecast.forecastday[i].day.mintemp_f} °</h6>
+              <h6>Humidity: ${weather.forecast.forecastday[i].day.avghumidity}</h6>
+              <h6>Windspeed: ${weather.forecast.forecastday[i].day.maxwind_mph}mph</h6>
             </div>
           </div>
         </div>
@@ -123,14 +112,9 @@ document.getElementById('searchCity').addEventListener('click', event => {
             document.getElementById('displayWeather').append(weatherElem)
           } 
         })
-      document.getElementById('city').value = ''
+      document.getElementById('city').value = ' '
     })
-  })
 
-
-  // < img src = "http://openweathermap.org/img/w/${listInfo.weather[0].icon}.png" >
-
-  // < img src = ${ current.condition.icon }></img >
 
 
 
